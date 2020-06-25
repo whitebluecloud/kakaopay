e03f8ee257016f9c1aa18b268a8ba3a50a78eb92
bluecloud <bluecloud0429@gmail.com>
2020-06-25 19:36:52 +0900

    kakaopay test spread project init

---
+++ src/main/java/com/kakaopay/spread/SpreadApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.spread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpreadApplication.class, args);
+    }
+
+}
