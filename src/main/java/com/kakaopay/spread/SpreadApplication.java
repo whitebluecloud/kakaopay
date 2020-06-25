6098eb46900816bda03994cabe20f39af85a1760
bluecloud <bluecloud0429@gmail.com>
2020-06-25 19:56:02 +0900

    어플리케이션 기본 세팅 추가

--- src/main/java/com/kakaopay/spread/SpreadApplication.java
+++ src/main/java/com/kakaopay/spread/SpreadApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpreadApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpreadApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpreadApplication.class, args);
+  }
 
 }
--- src/test/java/com/kakaopay/spread/SpreadApplicationTests.java
+++ src/test/java/com/kakaopay/spread/SpreadApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpreadApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
