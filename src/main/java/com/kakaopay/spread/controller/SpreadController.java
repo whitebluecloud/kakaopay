bf247f7efbc398d514bfe6669ed8e4a53717640e
bluecloud <bluecloud0429@gmail.com>
2020-06-27 00:18:33 +0900

    주석 제거

--- src/main/java/com/kakaopay/spread/controller/SpreadController.java
+++ src/main/java/com/kakaopay/spread/controller/SpreadController.java
@@ -30,13 +30,6 @@ public class SpreadController {
     return spreadResponseDto;
   }
 
-  /**
-   * TODO 뿌리기 시 발급된 token을 요청값으로 받습니다.
-   * TODO token에 해당하는 뿌리기 건 중 아직 누구에게도 할당되지 않은 분배건 하나를 API를 호출한 사용자에게 할당하고, 그 금액을 응답값으로 내려줍니다.
-   * TODO 뿌리기 당 한 사용자는 한번만 받을 수 있습니다. TODO 자신이 뿌리기한 건은 자신이 받을 수 없습니다.
-   * TODO 뿌리기가 호출된 대화방과 동일한 대화방에 속한 사용자만이 받을 수 있습니다.
-   * TODO 뿌린 건은 10분간만 유효합니다. 뿌린지 10분이 지난 요청에 대해서는 받기 실패 응답이 내려가야 합니다.
-   */
   @PutMapping("/spread/{token}")
   public void receiveSpreadMoney(@RequestHeader(name = "X-USER-ID") long userId,
                                  @RequestHeader(name = "X-ROOM-ID") String roomId,
